8e4f7263a9dd8fcc34032903e64eb854881cd283
unknown <belonyfdd90@gmail.com>
2025-08-09 09:15:49 -0700

    Modification

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+const js = `const companyName = "Mon Entreprise";`; // Exemple de code à tester
+let mostProfitableNeighborhood = "Chelsea";
+let companyCeo = "Susan Smith";
+
